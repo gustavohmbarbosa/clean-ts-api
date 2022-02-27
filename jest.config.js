e12f5717424d937a1adedb15acbfef068857d76f
Gustavo Melo <gustavobarbosa-18@hotmail.com>
2022-02-27 16:42:56 -0300

    chore: update husky and jest and lint-staged

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,20 @@
-export default {
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
   // A list of paths to directories that Jest should use to search for files in
   roots: [
     '<rootDir>/src'
   ],
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // Indicates which provider should be used to instrument code for coverage
-  // coverageProvider: 'v8'
-
-  // The test environment that will be used for testing
-  testEnvironment: 'node',
 
   // A map from regular expressions to paths to transformers
   transform: {
